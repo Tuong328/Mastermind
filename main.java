7193b8db01891da2730e71b929b13907b9eb0156
James Tuong <jtuong@live.unc.edu>
2021-05-21 14:36:04 -0400

    Turning the blackList of characters into a whiteList to save space and remove some edge cases

--- main.java
+++ main.java
@@ -1,32 +1,32 @@
 import coloredPegs.*;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class main {
 
     public static void main(String args[]) {
-       /** Rules: A random board of four colors is generated
-        * from a total of 6 different colors. The objective
-        * of the game is to guess what the order of the colors
-        * are in 6 tries. With each guess, if you chose a correct
-        * color and it is in the correct spot, you will get a black peg,
-        * if you get a white peg, that means that you have a correct
-        * color, but it is not in the right spot.
-        * Colors in this variation of game are: blue, orange, green, pink, yellow, red,
-        * We are using the first letter of the color: b, o, g, p, y, r
-        * Example Game: red -> red -> blue -> orange
-        *
-        * Example Guess 1: borb, will return 4 white pegs;
-        * Guess 1 Explanation: user chose the right colors, but they are
-        * not in the right order/position;
-        *
-        * Example Guess 2: rrbb, will return 3 black pegs
-        * Guess 2 Explanation: red, red, and blue are the correct guesses,
-        * but the last color is orange;
-        *
-        * **/
-
+        /** Rules: A random board of four colors is generated
+         * from a total of 6 different colors. The objective
+         * of the game is to guess what the order of the colors
+         * are in 6 tries. With each guess, if you chose a correct
+         * color and it is in the correct spot, you will get a black peg,
+         * if you get a white peg, that means that you have a correct
+         * color, but it is not in the right spot.
+         * Colors in this variation of game are: blue, orange, green, pink, yellow, red,
+         * We are using the first letter of the color: b, o, g, p, y, r
+         * Example Game: red -> red -> blue -> orange
+         *
+         * Example Guess 1: borb, will return 4 white pegs;
+         * Guess 1 Explanation: user chose the right colors, but they are
+         * not in the right order/position;
+         *
+         * Example Guess 2: rrbb, will return 3 black pegs
+         * Guess 2 Explanation: red, red, and blue are the correct guesses,
+         * but the last color is orange;
+         *
+         * **/
 
 
         //Board is a linked list of pegs, and this for loop generates our random board;
@@ -36,9 +36,9 @@ public class main {
         }
         System.out.println("Board generated, Good Luck!");
 
-        /*This for loop gives us our 4 colors in text if we want them
-        for (int i = 0; i < 4; i++) {
-          System.out.println(pegBoard.get(i).color());
+        //This for loop gives us our 4 colors in text if we want them
+       /* for (int i = 0; i < 4; i++) {
+            System.out.println(pegBoard.get(i).color());
         }*/
 
         System.out.println("Take your guess");
@@ -58,14 +58,21 @@ public class main {
          * they need to know if they have the right
          * color pegs or if they are in the right spot**/
         //String can only contain: b, o, g, p, y, r
-        char[] blackList = {'q', 'w', 'e', 't', 'u', 'i', 'a', 's', 'd', 'f', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'n', 'm'};
+        ArrayList<Character> whiteList = new ArrayList<>();
+        whiteList.add('b');
+        whiteList.add('o');
+        whiteList.add('g');
+        whiteList.add('p');
+        whiteList.add('y');
+        whiteList.add('r');
+
 
         String input = "";
         int guessesLeft = 6;
         while (guessesLeft > 0) {
             input = scanner.nextLine();
             //Invalid Input;
-            if (input.length() != 4 || inputCheck(input, blackList)) {
+            if (input.length() != 4 || !inputCheck(input, whiteList)) {
                 System.out.println("Invalid guess, you do not lose any attempts");
                 guessesLeft++;
             } else if (input.equals(correctAnswer)) {
@@ -88,15 +95,15 @@ public class main {
     }
 
     //Helper methods
-    static boolean inputCheck(String input, char[] blackList) {
-        for (char v: blackList) {
-            for (int i = 0; i < input.length(); i++) {
-                if (input.charAt(i) == v) {
-                    return true;
-                }
+    static boolean inputCheck(String input, ArrayList<Character> whiteList) {
+        boolean isValid = true;
+        for (int i = 0; i < input.length(); i++) {
+            if (!whiteList.contains(input.charAt(i))) {
+                isValid = false;
             }
         }
-        return false;
+
+        return isValid;
     }
 
 
